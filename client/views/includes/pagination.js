bfe05bb379ea2f9c6174be1c3af5ad228da34f53
nikitasmall <nikitasosnov@yahoo.com>
2015-08-07 12:48:01 +0300

    universal pagination implemented

--- client/views/includes/pagination.js
+++ client/views/includes/pagination.js
@@ -2,12 +2,12 @@ Template.pagination.helpers({
   count: function() {
     return Math.floor(Posts.find().count() / 5) + 1
   },
-  show: function() {
-    return (Posts.find().count() / 5) > 1
+  show: function(collection) {
+    return (collection.count() / 5) > 1
   },
-  paginator: function() {
+  paginator: function(collection) {
     var paginator = '<div>';
-    for (var i = 1; i <= Math.floor(Posts.find().count() / 5) + 1; i++) {
+    for (var i = 1; i <= Math.floor(collection.count() / 5) + 1; i++) {
       paginator += '<a href="/page/' + i + '">' + i + '</a>';
     }
     paginator += '</div>';
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -2,5 +2,8 @@ Template.postsList.helpers({
   posts: function() {
     var page = Router.current().params._page;
     return Posts.find({}, {limit: 5, skip: 5 * (page - 1)});
+  },
+  postCollection: function() {
+    return Posts.find();
   }
 });
