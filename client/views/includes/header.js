2df5b0514ab1446dc078e13dd0b20cf2dbf97c67
nikitasmall <nikitasosnov@yahoo.com>
2015-08-08 14:44:22 +0300

    minor fixes about login

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,5 +1,7 @@
-Template.header.helpers({
+// you don't need it. Just use currentUser helper at all the templates
+
+/*Template.header.helpers({
   user: function(){
     return !! Meteor.user();
   }
-});
+});*/
