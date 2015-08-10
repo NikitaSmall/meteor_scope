016fc78bcbea266eb899b63a2e8f6e791aae330d
nikitasmall <nikitasosnov@yahoo.com>
2015-08-10 11:54:19 +0300

    make errors seen

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -3,6 +3,11 @@ Errors = new Meteor.Collection(null);
 
 throwError = function(error) {
   Errors.insert({
-    message: error
+    message: error,
+    seen: false
   });
+}
+
+clearErrors = function() {
+  Errors.remove({seen: true});
 }
